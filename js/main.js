69b9d0d7edc5394ca877661769ebc55fa6096618
kevinjj <98082057+ke-wordaa@users.noreply.github.com>
2025-04-30 12:15:34 +0800

    114_4_30_1_歸零問題

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 $(document).ready(function () 
 {
-    let day_time = new Date('4/25/2026')
+    let day_time = new Date('4/26/2025')
     let  da = new Date()
-    let A= Math.abs(day_time-da)
-    day = Math.ceil(A/(1000 * 3600 * 24))
-    
-    days = Math.abs(day)
-    // days = 
-    console.log(days)
-    if (days >0)
+    if (day_time>da)
     {
-        $("#day").html(days+"天");
+        let A= Math.abs(day_time-da)
+        day = Math.ceil(A/(1000 * 3600 * 24))   
+
+        days = Math.abs(day)
+        // days = 
+        console.log(days)
+        if (days >0)
+        {
+            $("#day").html(days+"天");
+        }
+        else
+        {
+            $(h1).html('');
+        }
+    
+        if (days<=10) 
+        {
+            $("#day").css('font-size', '18vh');
+            $("#day").css('color', 'red');
+            // $('.public').css('top', '-20%');
+        }
     }
     else
     {
-        $(h1).html('');
+        $("#h1").html('');
+        // $(".h3_text").html('2025統測<br>已經過了喔!<br><a class="link" href=\'https://a2026.mykevinjj.dpdns.org\'>前往2026版</a>');
+        // $('.public').css('top', '-20%');
     }
-  
-    if (days<=10) 
-    {
-        $("#day").css('font-size', '23vh');
-        $("#day").css('color', 'red');
-        $('.public').css('top', '-5%');
-    }
-    }
+}
 );
